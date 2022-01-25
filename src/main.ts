fd0481416723232d47e8547996d13f37ac722720
Male <okuboy@163.com>
2022-01-25 18:04:20 +0800

    Revert "feat: 调整配置和优化代码"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import piniaStore from './store'
-import './index.css'
+/*
+ * @GitHub: https://github.com/MaleWeb/fast-vue3
+ * @version:
+ * @Author: 扫地盲僧
+ * @Date: 2022-01-19 16:19:27
+ * @LastEditors: BlindMonk
+ * @LastEditTime: 2022-01-24 17:24:56
+ */
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import piniaStore from "./store"
+import "./index.css"
 // import ArcoVue from "@arco-design/web-vue"
 // 支持markdown 语法高亮
 import 'prismjs'
@@ -12,5 +20,6 @@ import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-markup'
 import 'prismjs/components/prism-markup-templating'
 // 支持SVG
-import 'virtual:svg-icons-register'
-createApp(App).use(router).use(piniaStore).mount('#app')
+import "virtual:svg-icons-register"
+
+createApp(App).use(router).use(piniaStore).mount("#app")
