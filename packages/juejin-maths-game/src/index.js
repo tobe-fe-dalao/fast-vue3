77c113dbbc8c9ab0042a37fa7fbb62ca891406f0
MaleWeb <896226896@qq.com>
2022-03-05 10:21:21 +0800

    🐞 fix:
    升级算法：性能提升30%，检索速度提升100%，防止死循环，增加正解限制等

--- packages/juejin-maths-game/src/index.js
+++ packages/juejin-maths-game/src/index.js
@@ -5,7 +5,7 @@ const { operatorArr } = require('./lib/utils')
 const message = require('./lib/message')
 
 //自动模式
-const autoRunning = async () => {
+const autoRunning = async (needNum) => {
     //1,拿到关卡数据
     const res = new Game(USERID, COOKIE)
     const resList = await res.openGame()
@@ -16,47 +16,18 @@ const autoRunning = async () => {
 
     //3, 执行算法解析
     const getMatch = new Maths(nums, options, target)
-    getMatch.calcSum()
 
-    //查询正确答案和前50
-    const objs = Object.values(getMatch.cache)
-    let resArr = []
-    objs.forEach(val => {
-        //正确答案和前50
-        if (val.value === target && resArr.length < RESULT_COUNT) {
-            //尽可能过滤出正确的答案
-            val.formula = val.formula.toString()
-            if (nums.every(num => { return val.formula.indexOf(num) != -1 })) {
-                resArr.push(val)
-            }
-        }
-    })
-    console.log(resArr)
-
-    //封装到钉钉
-    // message(resArr.toString())
+    //5,前五个答案
+    getMatch.run(needNum)
 }
 //手动模式
 const handleRunning = async (nums, options, target) => {
     const getMatch = new Maths(nums, options, target)
-    getMatch.calcSum()
-    //查询正确答案和前50
-    const objs = Object.values(getMatch.cache)
-    let resArr = []
-    objs.forEach(val => {
-        //正确答案和前50
-        if (val.value === target && resArr.length < 50) {
-            //尽可能过滤出正确的答案
-            val.formula = val.formula.toString()
-            if (nums.every(num => { return val.formula.indexOf(num) != -1 })) {
-                resArr.push(val)
-            }
-        }
-    })
-    console.log(resArr)
+    getMatch.run(5)
 }
-// autoRunning()
-handleRunning([2, 1], ['*'], 2)
+autoRunning(5)
+
+// handleRunning([1, 4, 5], ['+', '*'], 21)
 
 
 
--- packages/juejin-maths-game/src/lib/Maths.js
+++ packages/juejin-maths-game/src/lib/Maths.js
@@ -1,4 +1,16 @@
-
+const colors = require('colors');
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+});
 class Maths {
     //解析算法部分-升级改造版   源地址：https://juejin.cn/post/7067862481800003591
     constructor(nums, options, target) {
@@ -6,6 +18,8 @@ class Maths {
         this.options = options;
         this.target = target;
         this.cache = {}
+        this.type = 2;//第二代
+        this.needNum = 5;//正确解的数量
     }
     calc(a, option, b) {
         let res;
@@ -41,32 +55,45 @@ class Maths {
             formula: '(' + formula + ')',
         };;
     };
-    calcSum() {
+    calcSum = () => {
         let cha = this.nums.length - 1 - this.options.length;
+        this.cache = {};
         if (cha) {
             this.options.push(...'.'.repeat(cha))
         }
+        this.nowNum = 0
         this.options.sort()
-        for (let i of this.nums) {
-            this.cache[i] = {
-                value: i,
-                formula: i,
+        let len = this.nums.length
+        this.nums.sort()
+        for (let i = 0; i < len; i++) {
+            let num = this.nums[i]
+            this.nums[i] = {
+                value: num,
+                formula: '' + num,
             }
         }
         this.calcLoop(this.nums, this.options, this.target)
-
+        this.cache = null;
     };
-    calcLoop(nums, options, target) {
-        if (nums.length === 1 && options.length == 0 && this.cache[nums[0]].value == target) {
-            // console.log(nums[0]);
-            return;
+    calcLoop = (nums, options, target) => {
+        let cache = this.cache
+        if (nums.length === 1 && options.length == 0 && nums[0].value == target) {
+            console.log(colors.warn(`算法破解：${nums[0].formula}`));
+            this.nowNum++;
+            if (this.nowNum >= this.needNum) {
+                console.log(colors.info('扫地盲僧第三代算法--运行结束--!'))
+                return true
+            }
+            return// nums[0]
         }
-        nums.sort()
-        let key = [...nums].join() + '|' + nums.join();
-        if (this.cache[key]) {
+
+        let nums2 = nums.map((v) => v.value)
+        nums2.sort()
+        let key = nums2.join() + '|' + options.join();
+        if (cache[key]) {
             return
         }
-        this.cache[key] = true
+        cache[key] = true
         for (let i of '*+-/.') {
             let index = options.indexOf(i);
             if (index >= 0) {
@@ -75,26 +102,24 @@ class Maths {
                 let len = nums.length
                 for (let j = 0; j < len - 1; j++) {
                     let newNums = [...nums];
-                    newNums.splice(nums.indexOf(nums[j]), 1);
+                    newNums.splice(j, 1);
                     for (let k = j + 1; k < len; k++) {
                         let newNums2 = [...newNums];
-                        newNums2.splice(newNums2.indexOf(nums[k]), 1);
-                        let newNum = this.calc(this.cache[nums[j]], i, this.cache[nums[k]])
+                        //newNums2.splice(newNums2.indexOf(nums[k]), 1);
+                        let newNum = this.calc(nums[j], i, nums[k])
                         if (isNaN(newNum.value)) {
                         }
                         else {
-                            this.cache[newNum.formula] = newNum;
-                            let t = [newNum.formula, ...newNums2];
-                            this.calcLoop(t, newOptions, target);
+                            newNums2[k - 1] = newNum//[newNum.formula, ...newNums2]
+                            if (this.calcLoop(newNums2, newOptions, target)) return true;
                         }
                         if (i != '+' && i != '*') {
-                            let newNum = this.calc(this.cache[nums[k]], i, this.cache[nums[j]]);
+                            let newNum = this.calc(nums[k], i, nums[j]);
                             if (isNaN(newNum.value)) {
                             }
                             else {
-                                this.cache[newNum.formula] = newNum;
-                                let t = [newNum.formula, ...newNums2];
-                                this.calcLoop(t, newOptions, target);
+                                newNums2[k - 1] = newNum
+                                if (this.calcLoop(newNums2, newOptions, target)) return true;
                             }
                         }
                     }
@@ -102,6 +127,10 @@ class Maths {
             }
         }
     }
+    run = (needNum = 5) => {
+        this.needNum = needNum;
+        this.calcSum(this.nums, this.options, this.target)
 
+    }
 }
 exports.Maths = Maths
