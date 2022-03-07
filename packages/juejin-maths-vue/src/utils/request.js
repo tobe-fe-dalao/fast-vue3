f787a11d232dcdfd183391121cc29c2abe96cdcf
wangscaler <wangscaler@163.com>
2022-03-07 09:01:11 +0800

    style(request): 移除token

--- packages/juejin-maths-vue/src/utils/request.js
+++ packages/juejin-maths-vue/src/utils/request.js
@@ -7,8 +7,7 @@ const service = axios.create({
 const tokenHeader = "authorization"; // token自定义头部名称
 service.interceptors.request.use(
   (config) => {
-    const cmq = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsInNvdXJjZSI6Imp1ZWppbiJ9.eyJleHBpcmVBdCI6MTY0MzIwNDMwMSwidXNlcklkIjoiMzUwMTMwOTQzODQ2NjQ2MiIsImlhdCI6MTY0MDYxMjMwMSwiZXhwIjoxNjQzMjA0MzAxfQ.oWghlcpjTam2q-v-ZQsKRp_s0tBd4MXdYUpyrVIwFlx3-jGkaXOAFhOXiJzEpAnqhrGlx5gBcvZcl_MpDyCXtkhCPbhYI23Xp6JSiAfMV6rgpeWPX632uAxdcpHutqJKkJPvGi5J430AgKp8SNpgdrcWOunpvfqPRtZtNlWvpLlrYmtwxATKHPUyXrCW-WgFYP0RtJYI2-5JK8I8UMO4tIFInVZcrTtW8T0BQul-1jtRAElp88pXoZ4ezjHU2gwsp_Mp0y7lDqwqYxlE2f1bW-7GP1abY1Rv0Y-SOweHGj9ErzFtmGJiZoT4myI38e-kjl7yDzRT-99qMDTA4U05zg";
-    const token = localStorage.getItem('token') || cmq;
+   const token = localStorage.getItem('token');
     if (token) {
       /* eslint-disable no-param-reassign */
       config.headers[tokenHeader] = token;
